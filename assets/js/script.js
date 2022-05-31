1a8b3eb3748c241d9bbec11d52579e079d245057
Hapiz <mhapiz10@gmail.com>
2022-05-31 20:08:41 +0800

    31 May 2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,31 @@
+(function () {
+  let scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+  });
+  // Navigation Menu  
+  let last_scroll_top = 0;
 
+  scroll.on("scroll", (position) => {
+    let scroll_top = position.scroll.y;
+    if (scroll_top > last_scroll_top) {
+      $('nav').addClass('hide-navbar');
+      $('nav').removeClass('show-navbar');
+      $('.nav-links').removeClass('show-links');
+    } else {
+      $('nav').addClass('show-navbar');
+      $('nav').removeClass('hide-navbar');
+      $('.nav-links').removeClass('show-links');
+    }
+    last_scroll_top = scroll_top;
 
-
-
-
-
-
-
-
-
-
-
-
-
-//  FOR NAVBAR
-// let last_scroll_top = 0;
-
-// $(window).on('scroll', function () {
-//   let scroll_top = window.pageYOffset;
-//   console.log(scroll_top);
-//   if(scroll_top > last_scroll_top) {
-//     // if scroll down
-//     $('nav').addClass('hide-navbar');
-//     $('nav').removeClass('show-navbar');
-//     $('.nav-links').removeClass('show-links');
-
-//   }
-//   else {
-//     // if scroll up
-//     $('nav').addClass('show-navbar');
-//     $('nav').removeClass('hide-navbar');
-//     $('.nav-links').removeClass('show-links');
-
-//   }
-//   last_scroll_top = scroll_top;
- 
-//   if ($(window).scrollTop()) {
-//     $('nav').addClass('sticky');
-//   } else {
-//     $('nav').removeClass('sticky');
-//   }
-  
-// });
+    if (scroll_top > 75) {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, true);
+})();
 
 const toggleBtn = document.getElementById('toggle-btn');
     const navLinks = document.getElementById('nav-links');
