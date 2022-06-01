f7ff328398c6a07648900d0b02a1b29c4535bb32
Hapiz <mhapiz10@gmail.com>
2022-06-01 14:24:31 +0800

    1 juni

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function () {
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader() {
+  $("#loadingDiv").fadeOut(500, function () {
+    // fadeOut complete. Remove the loading div
+    $("#loadingDiv").remove(); //makes page more lightweight 
+  });
+}
+
 (function () {
   let scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
+    smartphone: {
+      smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
   });
   // Navigation Menu  
   let last_scroll_top = 0;
